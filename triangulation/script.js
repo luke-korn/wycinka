e4fa9b2e4496acc62eca1b64c57eb76f0b663682
Luke_Korn <b66lz6bub@gmail.com>
2025-02-25 13:02:42 +0000

    test

--- triangulation/script.js
+++ triangulation/script.js
@@ -18,16 +18,15 @@ function main() {
 
 function onOrientationChange(event) {
   const info = document.querySelector("#heightInfo") 
+  const test = document.querySelector("#test") 
   let angle = event.beta - 90
+  test.innerHTML = angle
   if (angle < 0) angle = 0
   const slider = document.querySelector('#slider')
   const label = document.querySelector('#label')
-  console.log(slider.value) 
   label.innerHTML = "Distance: " + slider.value
   const distance = slider.value
   const height = Math.tan(angle * Math.PI / 180) * distance
-  console.log('height;', height)
   info.innerHTML = height.toFixed(1) + ' m (' + angle.toFixed(1) + '&deg;)'
-  console.log(height * 20)
 }
 
